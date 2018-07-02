9244578e2b690d98f5ac44c87c29e92b83d003c8
Jaremy Creechley <creechley@gmail.com>
2018-07-01 19:32:35 -0600

    simplifying test to only one guard

--- src/tests/test_guard_repeat.c
+++ src/tests/test_guard_repeat.c
@@ -10,28 +10,20 @@ enum events
 };
 
 static bool flag_guard1_called = false;
-static bool flag_guard2_called = false;
 static bool flag_action1_called = false;
-static bool guard2_ret_val = true;
+static bool guard1_ret_val = true;
 
 static void reset_test_flags(void)
 {
     flag_guard1_called = false;
-    flag_guard2_called = false;
     flag_action1_called = false;
-    guard2_ret_val = true;
+    guard1_ret_val = true;
 }
 
 static bool guard1_f(void)
 {
     flag_guard1_called = true;
-    return true;
-}
-
-static bool guard2_f(void)
-{
-    flag_guard2_called = true;
-    return guard2_ret_val;
+    return guard1_ret_val;
 }
 
 static void action1_f(void) { flag_action1_called = true; }
@@ -39,10 +31,12 @@ static void action1_f(void) { flag_action1_called = true; }
 static struct ufsm_state A;
 static struct ufsm_region region1;
 
-static struct ufsm_state simple_INIT = {.name = "Init",
-                                        .kind = UFSM_STATE_INIT,
-                                        .parent_region = &region1,
-                                        .next = &A};
+static struct ufsm_state simple_INIT = {
+    .name = "Init",
+    .kind = UFSM_STATE_INIT,
+    .parent_region = &region1,
+    .next = &A
+};
 
 static struct ufsm_state B = {
     .name = "State B",
@@ -58,15 +52,8 @@ static struct ufsm_state A = {
     .next = &B,
 };
 
-static struct ufsm_guard guard2;
-
 static struct ufsm_guard guard1 = {
     .f = &guard1_f,
-    .next = &guard2,
-};
-
-static struct ufsm_guard guard2 = {
-    .f = &guard2_f,
     .next = NULL,
 };
 
@@ -109,7 +96,8 @@ static struct ufsm_region region1 = {.state = &simple_INIT,
 static struct ufsm_machine m = {
     .name = "Simple Test Machine",
     .region = &region1,
-};
+}
+;
 
 int main(void)
 {
@@ -117,35 +105,38 @@ int main(void)
 
     reset_test_flags();
     test_init(&m);
+
+    // Init SM
     err = ufsm_init_machine(&m);
     assert(err == UFSM_OK && "Initializing");
     assert(m.region->current == &A);
     assert(flag_guard1_called == false);
-    assert(flag_guard2_called == false);
     assert(flag_action1_called == false);
 
+    // Trans from A -> B
     reset_test_flags();
     err = ufsm_process(&m, EV_B);
     assert(m.region->current == &B && err == UFSM_OK);
 
+    // Trans from B -> A
     reset_test_flags();
     err = ufsm_process(&m, EV_A);
     assert(m.region->current == &A && err == UFSM_OK);
     assert(flag_guard1_called);
-    assert(flag_guard2_called);
     assert(flag_action1_called);
 
+    // Trans from A -> B
     reset_test_flags();
     err = ufsm_process(&m, EV_B);
     assert(m.region->current == &B && err == UFSM_OK);
 
+    // Trans from B -> A (false)
     reset_test_flags();
-    guard2_ret_val = false;
+    guard1_ret_val = false;
     err = ufsm_process(&m, EV_A);
     assert(m.region->current == &B);
     assert(err == UFSM_OK);
     assert(flag_guard1_called);
-    assert(flag_guard2_called);
     assert(flag_action1_called == false);
 
     return 0;
